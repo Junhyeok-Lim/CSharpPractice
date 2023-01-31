cc01462692f77aa10a948d2f1b00aafe181fd9ee
Junhyeok-Lim <junh0301@naver.com>
2023-01-31 15:17:13 +0900

    초기 커밋.

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
